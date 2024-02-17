e2b0111763387c0091ad333d80560b78fc13a3b6
Sam Arustamyan <samvel.arustamyan@gmail.com>
2024-02-18 01:51:25 +0400

    Initial setup

---
+++ Technolab.OnlineLibrary.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Technolab.OnlineLibrary.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
