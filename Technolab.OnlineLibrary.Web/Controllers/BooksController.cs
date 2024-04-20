5bc923426c5eb9c22818f02b3381b3be8eba3d51
Sam Arustamyan <samvel.arustamyan@gmail.com>
2024-04-20 13:50:27 +0400

    Add the unit test project

--- Technolab.OnlineLibrary.Web/Controllers/BooksController.cs
+++ Technolab.OnlineLibrary.Web/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Technolab.OnlineLibrary.Web.Models;
 using Technolab.OnlineLibrary.Web.ViewModels;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Technolab.OnlineLibrary.Web.Controllers
 {
@@ -13,13 +14,13 @@ namespace Technolab.OnlineLibrary.Web.Controllers
             this.ContextFactory = contextFactory;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string searchTerm)
         {
-            using var context = ContextFactory.Create();
+            var books = Search(searchTerm);
 
             var model = new BookSearchViewModel
             {
-                Books = context.Books.Take(10).Select(x => new BookViewModel
+                Books = books.Select(x => new BookViewModel
                 {
                     Id = x.Id,
                     Title = x.Title,
@@ -31,5 +32,19 @@ namespace Technolab.OnlineLibrary.Web.Controllers
         }
 
         public ILibraryDbContextFactory ContextFactory { get; }
+
+        [NonAction]
+        public List<Book> Search(string searchTerm)
+        {
+            using var context = ContextFactory.Create();
+
+            var books = (IEnumerable<Book>)context.Books;
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                var searchTerms = searchTerm.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                books = books.Where(book => searchTerms.Any(term => book.Title.Contains(term) || book.Author.Contains(term)));
+            }
+            return books.ToList();
+        }
     }
 }
